1aae21a710031246d90716d8583c69c48b2a6953
James Schloss <jrs.schloss@gmail.com>
2024-08-06 00:02:34 +0200

    Adding sketch for buffers and contexts

---
+++ include/quibble_context.h
@@ -0,0 +1,28 @@
+#ifndef QUIBBLE_CONTEXT_H
+#define QUIBBLE_CONTEXT_H
+
+#include "errors.h"
+#include "macros.h"
+#include <CL/cl.h>
+
+struct quibble_context{
+    cl_platform_id platform_id;
+    cl_device_id device_id;
+    cl_uint num_devices;
+    cl_uint num_platforms;
+    cl_context context;
+    cl_command_queue command_queue;
+    cl_program program;
+    cl_kernel kernel;
+    size_t global_item_size;
+    size_t local_item_size;
+};
+
+struct quibble_context create_default_context();
+/*
+void list_devices(...);
+void choose_device(...);
+struct quibble_context create_context(int device, ...);
+*/
+
+#endif
---
+++ src/quibble_context.c
@@ -0,0 +1,46 @@
+/*-------------quibble_context.c----------------------------------------------//
+
+Purpose: This file defines the quibble_context and associate functions.
+         A quibble_context holds all the necessary information for quibble
+         and OpenCL
+
+//----------------------------------------------------------------------------*/
+
+#include "../include/quibble_context.h"
+
+struct quibble_context create_default_context(){
+    struct quibble_context qbc;
+    qbc.platform_id = NULL;
+    qbc.device_id = NULL;
+
+    cl_int err;
+    cl_check(
+        clGetPlatformIDs(1, &qbc.platform_id, &qbc.num_platforms)
+    );
+    cl_check(
+        clGetDeviceIDs(qbc.platform_id,
+                       CL_DEVICE_TYPE_DEFAULT,
+                       1,
+                       &qbc.device_id,
+                       &qbc.num_devices)
+    );
+
+    qbc.context = clCreateContext(NULL, 1, &qbc.device_id, NULL, NULL, &err);
+    cl_check(err);
+
+    qbc.command_queue = clCreateCommandQueueWithProperties(
+        qbc.context,
+        qbc.device_id,
+        0,
+        &err
+    );
+    cl_check(err);
+
+    return qbc;
+}
+
+/*
+void list_devices(...);
+void choose_device(...);
+struct quibble_context create_context(int device, ...);
+*/
