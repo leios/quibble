8a71c2aedad6f82298a5b7c218c97db93d771886
James Schloss <jrs.schloss@gmail.com>
2024-08-18 17:07:49 +0200

    adding skeleton for quibble_verses

--- src/quibble_verses.c
+++ src/quibble_verses.c
@@ -4,3 +4,68 @@ Purpose: A quibble_verse is a user-submitted code fragment that will later be
          transformed into a working OpenCL kernel
 
 //----------------------------------------------------------------------------*/
+
+#include "../include/quibble_verses.h"
+
+void qb_replace_char(char *verse, int verse_size, char a, char b){
+}
+
+void qb_replace_char_if_proceeding(char *verse, int verse_size,
+                                   char *preamble, int preamble_size,
+                                   char a, char b){
+}
+
+void preprocess_verse(char *verse, int verse_size){
+    // replace all spaces by new lines
+    qb_replace_char(verse, verse_size, ' ', '\n');
+
+    // except for the arguments after some preprocessor options that need to be
+    // in the same line
+    qb_replace_char_if_proceeding(verse, verse_size, "#ifdef", 7, '\n', ' ');
+    qb_replace_char_if_proceeding(verse, verse_size, "#ifndef", 8, '\n', ' ');
+
+    // #define with two arguments will not work
+    qb_replace_char_if_proceeding(verse, verse_size, "#define", 7, '\n', ' ');
+
+    // don't leave any spaces in arguments
+    qb_replace_char_if_proceeding(verse, verse_size, "#if", 3, '\n', ' ');
+
+    // don't leave any spaces in arguments
+    qb_replace_char_if_proceeding(verse, verse_size, "#elif", 5, '\n', ' ');
+    qb_replace_char_if_proceeding(verse, verse_size, "#pragma", 7, '\n', ' ');
+}
+
+// Reads an input file and parses everything into verses or OCL functions
+quibble_program qb_create_program(char *filename);
+quibble_verse qb_find_verse(quibble_program qp, char *verse_name);
+quibble_verse qb_parse_verse(char *verse);
+quibble_keyword *qb_parse_keywords(char *preamble);
+
+// Configures preambles of existing verses
+void qb_configure_verse(quibble_verse *qv, ...);
+
+// An echo is a verse with the same body, but different preamble
+quibble_verse qb_echo_verse(quibble_verse qv, ...);
+
+// To be used in `qb_configure_verse` to create preamble string
+char *qb_create_preamble(quibble_verse qv){
+    
+}
+
+void qb_free_keyword(quibble_keyword qkwarg){
+    free(qkwarg.variable);
+    free(qkwarg.value);
+}
+
+void qb_free_keyword_array(quibble_keyword *qkwargs, int n){
+    for (int i = 0; i < n; ++i){
+        qb_free_keyword(qkwargs[i]);
+    }
+}
+
+void qb_free_verse(quibble_verse qv){
+    if (qv.echo == false){
+        free(qv.body);
+    }
+    qb_free_keyword_array(qv.kwargs, qv.num_kwargs);
+}
